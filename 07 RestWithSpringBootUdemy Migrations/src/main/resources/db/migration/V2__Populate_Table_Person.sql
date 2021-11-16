c79a3ad7b620fbbb8ec7201f8331300efc5e35b7
MVBPinho <marcos.vb.pinho@live.com>
2021-11-15 21:36:50 -0300

    Adicionando Suporte à Migrations

---
+++ 07 RestWithSpringBootUdemy Migrations/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` VALUES 
+	(1,'Uberlandia','Leandro','Male','Costa'),
+	(2,'Uberlandia','Gabriela','Famele','Costa'),
+	(8,'Uberlandia','Flavio','Famele','Costa'),
+	(9,'Uberlandia','Flavio','Famele','Costa');
